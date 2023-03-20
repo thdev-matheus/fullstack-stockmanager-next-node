4418f0c0fa810a75bef8dd6c561746bdce1c6ea8
Theus <matheusth.dev@gmail.com>
2023-03-20 17:02:54 -0300

    feat: sevice de criação de usuário

--- back/src/services/user/createUser.service.ts
+++ back/src/services/user/createUser.service.ts
@@ -1,5 +1,31 @@
 import { AppDataSource } from "../../data-source";
 import { AppError } from "../../errors";
 import { User } from "../../entities/user";
+import { IUserRequest, IUser } from "./types";
 
-export const createUserService = () => {};
+export const createUserService = async ({
+  isAdm,
+  name,
+  password,
+  securityAnswer,
+  securityAsk,
+}: IUserRequest): Promise<IUser> => {
+  const userRepo = AppDataSource.getRepository(User);
+  const userAlreadyExists = await userRepo.findOneBy({ name });
+
+  if (userAlreadyExists && userAlreadyExists.isActive) {
+    throw new AppError(409, "Usuário já existe no banco de dados");
+  }
+
+  const newUser = userRepo.create({
+    name,
+    password,
+    securityAnswer,
+    securityAsk,
+    isAdm,
+  });
+
+  await userRepo.save(newUser);
+
+  return newUser;
+};
