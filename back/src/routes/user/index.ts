36084125b179fdce7612818de6dc4edfdcf8901e
Theus <matheusth.dev@gmail.com>
2023-03-20 21:51:57 -0300

    feat: middleware de verificação de campos para criação de usuário

--- back/src/routes/user/index.ts
+++ back/src/routes/user/index.ts
@@ -2,10 +2,18 @@ import { Router } from "express";
 import { createUserController } from "../../controllers/user/createUser.controller";
 import { readAllUsersController } from "../../controllers/user/readAllUsers.controller";
 
+import { createUserValidationFieldsMiddleware } from "../../middlewares/createUserValidationFields.middleware";
+
+import { createUserSchema } from "../../schemas/createUser.schema";
+
 const router = Router();
 
 export const userRoutes = (): Router => {
-  router.post("", createUserController);
+  router.post(
+    "",
+    createUserValidationFieldsMiddleware(createUserSchema),
+    createUserController
+  );
   router.get("", readAllUsersController);
 
   return router;
