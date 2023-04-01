40ecb2ca2b4cc92c24c3dcae5a1bfa809b8f8add
Theus <matheusth.dev@gmail.com>
2023-04-01 17:01:06 -0300

    feat: rota de criação de categoria

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Express } from "express";
 import { sessionRoutes } from "./session";
 import { userRoutes } from "./user";
+import { categoryRoutes } from "./category";
 
 export const appRoutes = (app: Express): void => {
   app.use("/users", userRoutes());
   app.use("/login", sessionRoutes());
+  app.use("/categories", categoryRoutes());
 };
