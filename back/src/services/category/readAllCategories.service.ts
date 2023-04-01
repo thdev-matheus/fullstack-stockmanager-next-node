de98a3f1f797823c0eb6371c9bcd86eeec735755
Theus <matheusth.dev@gmail.com>
2023-04-01 19:38:21 -0300

    feat: service de leitura de todas as categorias

--- back/src/services/category/readAllCategories.service.ts
+++ back/src/services/category/readAllCategories.service.ts
@@ -4,6 +4,11 @@ import { Category } from "../../entities/category";
 export const readAllCategoriesService = async () => {
   const categoryRepo = AppDataSource.getRepository(Category);
   const categories = await categoryRepo.find();
+  const countCategories = await categoryRepo.count();
 
+  const response = {
+    counter: countCategories,
+    results: categories,
+  };
   return categories;
 };
