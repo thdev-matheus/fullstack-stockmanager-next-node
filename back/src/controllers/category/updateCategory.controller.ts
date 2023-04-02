66f8a3bee066820ef4ecf355595af3fd7c0cb964
Theus <matheusth.dev@gmail.com>
2023-04-02 10:22:59 -0300

    feat: controller de atualização de categoria

---
+++ back/src/controllers/category/updateCategory.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import { updateCategoryService } from "../../services/category/updateCategory.service";
+import { ICategoryRequest } from "../../types/category";
+
+export const updateCategoryController = async (req: Request, res: Response) => {
+  const { name }: ICategoryRequest = req.body;
+  const { categoryId } = req.params;
+
+  const updatedCategory = await updateCategoryService({ name }, categoryId);
+
+  return res.status(200).json(updatedCategory);
+};
