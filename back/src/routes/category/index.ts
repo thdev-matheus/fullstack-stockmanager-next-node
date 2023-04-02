fd18297fc3275d95e589a85772c69696810a8cf7
Theus <matheusth.dev@gmail.com>
2023-04-02 10:42:11 -0300

    feat: rota de deleção de categoria

--- back/src/routes/category/index.ts
+++ back/src/routes/category/index.ts
@@ -6,6 +6,7 @@ import { createCategoryController } from "../../controllers/category/createCateg
 import { readAllCategoriesController } from "../../controllers/category/readAllCategories.controller";
 import { retrieveCategoryController } from "../../controllers/category/retrieveCategory.controller";
 import { updateCategoryController } from "../../controllers/category/updateCategory.controller";
+import { deleteCategoryController } from "../../controllers/category/deleteCategory.controller";
 
 const router = Router();
 
@@ -19,6 +20,7 @@ export const categoryRoutes = () => {
 
   router.post("", createCategoryController);
   router.patch("/:categoryId", updateCategoryController);
+  router.delete("/:categoryId", deleteCategoryController);
 
   return router;
 };
