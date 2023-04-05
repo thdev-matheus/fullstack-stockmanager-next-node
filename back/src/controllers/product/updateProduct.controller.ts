ccf24f341172c44fc74e37b24f54fee8569ddf6a
Theus <matheusth.dev@gmail.com>
2023-04-05 14:47:21 -0300

    feat: controlador de atualização de produto

---
+++ back/src/controllers/product/updateProduct.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { updateProductService } from "../../services/product/updateProduct.service";
+import { IProductRequest } from "../../types/product";
+
+export const updateProductController = async (req: Request, res: Response) => {
+  const {
+    categoryName,
+    name,
+    purchasePrice,
+    salePrice,
+    stock,
+  }: IProductRequest = req.body;
+  const { productId } = req.params;
+
+  const updatedProduct = await updateProductService(
+    { categoryName, name, purchasePrice, salePrice, stock },
+    productId
+  );
+
+  return res.status(200).json(updatedProduct);
+};
