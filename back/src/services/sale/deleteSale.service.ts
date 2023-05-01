fda07489ff091964d92f073c45ecadd0ec747618
Theus <matheusth.dev@gmail.com>
2023-04-30 23:11:16 -0300

    feat: service de deleção de venda

---
+++ back/src/services/sale/deleteSale.service.ts
@@ -0,0 +1,30 @@
+import { AppDataSource } from "../../data-source";
+import { Sale } from "../../entities/sale";
+import { Product } from "../../entities/product";
+import { AppError } from "../../errors";
+
+export const deleteSaleService = async (saleId: string) => {
+  const saleRepo = AppDataSource.getRepository(Sale);
+  const productRepo = AppDataSource.getRepository(Product);
+
+  const sale = await saleRepo.findOne({
+    relations: { user: true, products: true },
+    where: { id: saleId },
+  });
+
+  if (!sale) {
+    throw new AppError(404, "venda não encontrada");
+  }
+
+  for (let i = 0; i < sale.products.length; i++) {
+    const sProd = sale.products[i];
+
+    await productRepo.update(sProd.product.id, {
+      stock: sProd.product.stock + sProd.quantity,
+    });
+  }
+
+  const deletedSale = await saleRepo.delete(saleId);
+
+  return deletedSale;
+};
