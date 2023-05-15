2ad4456859ed2d5b239d626530627af1d6f0f9af
Theus <matheusth.dev@gmail.com>
2023-05-15 17:23:34 -0300

    feat: lógica na leitura de todas as categorias

--- back/src/services/category/readAllCategories.service.ts
+++ back/src/services/category/readAllCategories.service.ts
@@ -1,14 +1,37 @@
 import { AppDataSource } from "../../data-source";
 import { Category } from "../../entities/category";
+import { Company } from "../../entities/company";
+import { AppError } from "../../errors";
 
-export const readAllCategoriesService = async () => {
+export const readAllCategoriesService = async (userCompanyId: string) => {
   const categoryRepo = AppDataSource.getRepository(Category);
-  const categories = await categoryRepo.find();
-  const countCategories = await categoryRepo.count();
+  const companyRepo = AppDataSource.getRepository(Company);
+  const company = await companyRepo.findOneBy({ id: userCompanyId });
 
-  const response = {
-    counter: countCategories,
-    results: categories,
-  };
-  return response;
+  if (!company) {
+    throw new AppError(404, "empresa ão localizada no banco de dados");
+  }
+
+  if (company.name === "stock manager staff") {
+    const categories = await categoryRepo.find();
+    const countCategories = await categoryRepo.count();
+
+    const response = {
+      counter: countCategories,
+      results: categories,
+    };
+    return response;
+  } else {
+    const categories = await categoryRepo.find({
+      where: { company: { name: company.name } },
+      relations: { company: true },
+    });
+
+    const response = {
+      counter: categories.length,
+      results: categories,
+    };
+
+    return response;
+  }
 };
