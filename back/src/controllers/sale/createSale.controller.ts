4dcbbdd6e95a17bcd58b56a4c572a11a0eb5847b
Theus <matheusth.dev@gmail.com>
2023-05-16 19:46:33 -0300

    refactor: controlador de criação de venda

--- back/src/controllers/sale/createSale.controller.ts
+++ back/src/controllers/sale/createSale.controller.ts
@@ -5,9 +5,9 @@ import { ISaleRequest } from "../../types/sale";
 
 export const createSaleController = async (req: Request, res: Response) => {
   const data: ISaleRequest = req.body;
-  const { userId } = req;
+  const { userId, userCompanyId } = req;
 
-  const sale = await createSaleService(data, userId!);
+  const sale = await createSaleService(data, userId!, userCompanyId!);
 
   return res.status(200).json(instanceToPlain(sale));
 };
