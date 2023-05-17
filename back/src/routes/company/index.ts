e0df88cf1f87230378e398652050de6d6f690d6c
Theus <matheusth.dev@gmail.com>
2023-05-16 22:37:28 -0300

    feat: rota de leitura de todas as empresas

--- back/src/routes/company/index.ts
+++ back/src/routes/company/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { isStaffOrOwnerMiddleware } from "../../middlewares/isStaffOrOwner.middleware";
 import { createCompanyController } from "../../controllers/company/createCompany.controller";
 import { isAuthenticatedMiddleware } from "../../middlewares/isAuthenticated.middleware";
+import { readAllCompaniesController } from "../../controllers/company/readAllCompanies.controller";
 
 const router = Router();
 
@@ -9,6 +10,7 @@ export const companyRoutes = () => {
   router.use(isAuthenticatedMiddleware);
 
   router.post("", isStaffOrOwnerMiddleware, createCompanyController);
+  router.get("", isStaffOrOwnerMiddleware, readAllCompaniesController);
 
   return router;
 };
