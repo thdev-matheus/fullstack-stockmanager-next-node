30be37ddc69a632ec0c8a76d1854f0ab0046d15f
Theus <matheusth.dev@gmail.com>
2023-05-18 14:21:41 -0300

    feat: rota de leitura de pergunta de segurança

--- back/src/routes/session/index.ts
+++ back/src/routes/session/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { userLoginController } from "../../controllers/session/userLogin.controller";
+import { retrieveSecurityAskController } from "../../controllers/session/retrieveSecurityAsk.controller";
 
 const router = Router();
 
 export const sessionRoutes = (): Router => {
   router.post("", userLoginController);
+  router.get("/security-ask/:userName", retrieveSecurityAskController);
 
   return router;
 };
