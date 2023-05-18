e99a25402e990b9cb8068a756239bfebdf8217d4
Theus <matheusth.dev@gmail.com>
2023-05-18 19:02:53 -0300

    feat: início da página de login

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
+import style from "./styles.module.scss";
+
 export default function LoginPage() {
   return (
-    <>
-      <h1>LoginPage</h1>
-    </>
+    <section className={style.container}>
+      <div className={style.container__box}>
+        <h1>Faça o seu login</h1>
+        <form>
+          <input type="text" />
+          <input type="text" />
+          <button type="submit">Entrar</button>
+        </form>
+        <p>
+          Não tem login?{" "}
+          <Link className={style.link} href="/request">
+            Solicite
+          </Link>{" "}
+          seu usuário
+        </p>
+      </div>
+    </section>
   );
 }
